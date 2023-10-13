6170750037d1eac68759460611f155b07b4f0312
Henna Mäkiluoma <henna.makiluoma@futurice.com>
2023-10-13 14:10:00 +0300

    Upgrade react & its dependants

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,18 +1,13 @@
 import React, { Component } from 'react'
-import styled from 'styled-components'
 
 import logo from '../logo.svg'
-
-const Wrapper = styled.a.attrs({
-    className: 'navbar-brand',
-})``
 
 class Logo extends Component {
     render() {
         return (
-            <Wrapper>
+            <div>
                 <img src={logo} width="50" height="50" />
-            </Wrapper>
+            </div>
         )
     }
 }
